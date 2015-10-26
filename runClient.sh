a4691cc27228aca5fd42ffee48a5361a25a16617
javaone <javaone@javaone-softarex-vm>
2015-10-25 21:25:23 -0700

    added scripts to simplify work with project

---
+++ runClient.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+java -jar jworker-demo-client/build/libs/jworker-demo-client-1.0.jar
+
---
+++ runServer.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+java -jar jworker-demo-server/build/libs/jworker-demo-server-1.0.jar 
+
