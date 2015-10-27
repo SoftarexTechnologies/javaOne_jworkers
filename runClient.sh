0782f3ede39c3fadbb88624b5b6d6d43a14b228d
Ivan Dubynets <ivan@infostroy.com.ua>
2015-10-27 06:53:11 +0200

    build project before start apps

--- runClient.sh
+++ runClient.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+gradle build
+
 java -jar jworker-demo-client/build/libs/jworker-demo-client-1.0.jar
 
--- runServer.sh
+++ runServer.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+gradle build
+
 java -jar jworker-demo-server/build/libs/jworker-demo-server-1.0.jar 
 
